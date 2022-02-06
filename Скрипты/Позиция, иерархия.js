7ddd4bcd8561a19d1f5036941061aaa581b7efce
Leancher <Leancher@yandex.ru>
2022-02-06 20:59:28 +0300

    (no message)

---
+++ Скрипты/Позиция, иерархия.js
@@ -0,0 +1,34 @@
+﻿function AddNamePanel(NumberUnit, panel) {
+    if (panel.ArtPos.indexOf('(') < 0)
+        panel.ArtPos = panel.Designation;
+}
+
+function AddNameBlock(block) {
+    if ((block.ArtPos.indexOf('(') < 0) && (block.ArtPos.length > 0))
+        block.ArtPos = block.Designation.slice(0,2);
+}
+
+function InToList(NumberUnit, obj) {
+    for (var i = 0; i < obj.Count; i++) {
+        objChild = obj.Objects[i];
+        if ((objChild instanceof TFurnBlock) || (objChild instanceof TLayer3D)) {
+            if (objChild instanceof TFurnBlock) AddNameBlock(objChild);
+            InToList(NumberUnit, objChild);
+        }
+        if (obj[i] instanceof TFurnPanel) {
+            AddNamePanel(NumberUnit, obj[i].AsPanel);
+        }
+    }
+}
+
+for (var i = 0; i < Model.Count; i++) {
+    obj = Model.Objects[i];
+    NumberUnit = obj.ArtPos;
+    if ((obj instanceof TFurnBlock) || (obj instanceof TLayer3D)) {
+        if (obj instanceof TFurnBlock) AddNameBlock(obj);
+        InToList(NumberUnit, obj);
+    }
+    if (obj instanceof TFurnPanel) {
+        AddNamePanel(NumberUnit, obj.AsPanel);
+    }
+}
