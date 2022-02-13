182b9909904013dbea7f249eda836ca83f8dc0e7
Leancher <Leancher@yandex.ru>
2022-02-13 21:40:08 +0300

    (no message)

--- Скрипты/Позиция, иерархия.js
+++ Скрипты/Позиция, иерархия.js
@@ -1,34 +1,41 @@
-﻿function AddNamePanel(NumberUnit, panel) {
+﻿﻿function AddNamePanel(panel) {
     if (panel.ArtPos.indexOf('(') < 0)
         panel.ArtPos = panel.Designation;
 }
 
 function AddNameBlock(block) {
-    if ((block.ArtPos.indexOf('(') < 0) && (block.ArtPos.length > 0))
-        block.ArtPos = block.Designation.slice(0,2);
+    if ((block.ArtPos.indexOf('(') < 0) && (block.ArtPos.length > 0)){
+        system.log("name: " + block.Name);
+    }
+        block.ArtPos = block.Designation;//.slice(0,2);
 }
 
-function InToList(NumberUnit, obj) {
+function InToList(obj) {
     for (var i = 0; i < obj.Count; i++) {
         objChild = obj.Objects[i];
         if ((objChild instanceof TFurnBlock) || (objChild instanceof TLayer3D)) {
-            if (objChild instanceof TFurnBlock) AddNameBlock(objChild);
-            InToList(NumberUnit, objChild);
+            if (objChild instanceof TFurnBlock) {
+                //system.log("name: " + objChild.Name);
+                AddNameBlock(objChild);                
+            }
+            InToList(objChild);
         }
         if (obj[i] instanceof TFurnPanel) {
-            AddNamePanel(NumberUnit, obj[i].AsPanel);
+            AddNamePanel(obj[i].AsPanel);
         }
     }
 }
 
 for (var i = 0; i < Model.Count; i++) {
     obj = Model.Objects[i];
-    NumberUnit = obj.ArtPos;
     if ((obj instanceof TFurnBlock) || (obj instanceof TLayer3D)) {
-        if (obj instanceof TFurnBlock) AddNameBlock(obj);
-        InToList(NumberUnit, obj);
+        if (obj instanceof TFurnBlock) {
+            system.log("level 1");
+            AddNameBlock(obj);            
+        }
+        InToList(obj);
     }
     if (obj instanceof TFurnPanel) {
-        AddNamePanel(NumberUnit, obj.AsPanel);
+        AddNamePanel(obj.AsPanel);
     }
 }
